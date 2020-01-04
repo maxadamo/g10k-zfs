0fa5ae54e186a3f6f6d5f8db2aae023006c56a6d
Massimiliano Adamo <maxadamo@gmail.com>
2020-01-05 00:40:28 +0100

    create RPM and DEB

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 BIN_NAME=g10k-zfs
 PATH=$PATH:$(go env GOPATH)/bin
 GOPATH=$(go env GOPATH)
+EMAIL="Massimiliano Adamo <maxadamo@gmail.com>"
+DESC="leverage ZFS snapshots for G10K "
 export BIN_NAME PATH GOPATH
 
 LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
@@ -16,8 +18,18 @@ git checkout $LATEST_TAG
 go build -ldflags "-s -w -X main.appVersion=${PROG_VERSION} -X main.buildTime=${BUILDTIME}" -o ./${BIN_NAME}
 
 if [ $? -gt 0 ]; then
-  echo -e "\nthere was an error while compiling the code\n"
-  exit
+    echo -e "\nthere was an error while compiling the code\n"
+    exit
 fi
 
 echo -e "\nthe binary was compiled and it is avilable as:\n - $(pwd)/${BIN_NAME}\n"
+
+# create RPM and DEB for amd64
+if which fpm; then
+    fpm -f -t rpm -n $BIN_NAME -v $PROG_VERSION --maintainer "$EMAIL" --vendor "$EMAIL" \
+        -a x86_64 --description "$DESC" -s dir ./${BIN_NAME}=/usr/local/bin//${BIN_NAME}
+    fpm -f -t deb -n $BIN_NAME -v ${PROG_VERSION}-1 --maintainer "$EMAIL" --vendor "$EMAIL" \
+        -a amd64 --description "$DESC" -s dir ./${BIN_NAME}=/usr/local/bin/${BIN_NAME}
+else
+    echo -e "if you want to create RPM and DEB packages, please install fpm:\n - gem install fpm"
+fi
